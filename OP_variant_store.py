db77f892f846997180b735366d38a8def650636b
Henri Hebeisen <henri.hebeisen@gmail.com>
2024-01-22 21:57:43 +0100

    Adding material management

--- OP_variant_store.py
+++ OP_variant_store.py
@@ -4,24 +4,46 @@ from bpy.types import Operator, PropertyGroup
 from bpy.props import BoolProperty, IntProperty, CollectionProperty, StringProperty
 from .utils import get_addon_prefs
 
-def get_object_properties(context, obj, variant_uuid):
+PROPS = 'props'
+MATS = 'mats'
+
+def get_object_properties(context, obj):
     result = {}
     properties = get_addon_prefs().object_properties_to_store.split(',')
     for prop in properties:
         prop = prop.strip()
-        try : 
+        try :
             result[prop] = getattr(obj, prop)
-        except: 
+        except:
             print(f'No {prop} property for {obj.name}')
-    obj[variant_uuid] =  result
+    return result
+
+def get_object_materials(context, obj):
+    result = []
+    for mat in obj.material_slots:
+        result.append(mat.name)
+    return result
 
 def set_object_properties(context, obj, variant_uuid):
-    try: 
-        properties = obj[variant_uuid]
-        for key, value in properties.items():
-            setattr(obj, key, value)
+    try:
+        stored_properties = obj[variant_uuid]
+        if PROPS in stored_properties:
+            for key, value in stored_properties[PROPS].items():
+                setattr(obj, key, value)
     except :
         print(f'Could not apply properties for {obj.name}')
+
+def set_object_materials(context, obj, variant_uuid):
+    try:
+        stored_properties = obj[variant_uuid]
+    except :
+        print(f'Could not apply materials for {obj.name}')
+        return None
+    if MATS not in stored_properties:
+        return None
+    
+    for index, material_name in enumerate(stored_properties[MATS]):
+        obj.material_slots[index].material = bpy.data.materials[material_name]
 
 class VariantItem(PropertyGroup):
     name: StringProperty(name="Variant Name", )
@@ -39,11 +61,15 @@ class VA_store_scene_variant(Operator):
     )
 
     def execute(self, context):
-        objects = context.selected_objects if self.only_selected else bpy.data.objects
+        objects = context.selected_objects if self.only_selected else context.scene.objects
         variant_UUID = str(uuid.uuid4())
 
         for obj in objects:
-            get_object_properties(context, obj,variant_UUID)
+            all = {}
+            all[PROPS] = get_object_properties(context, obj)
+            all[MATS] = get_object_materials(context, obj)
+
+            obj[variant_UUID] =  all
 
         new_var = context.scene.variants.add()
         new_var.name = f"Variant_{len(context.scene.variants)}"
@@ -64,6 +90,7 @@ class VA_apply_scene_variant(Operator):
         active_var = context.scene.variants[context.scene.active_variant]
         for obj in bpy.data.objects:
             set_object_properties(context, obj, active_var.uuid)
+            set_object_materials(context, obj, active_var.uuid)
         return {"FINISHED"}
 
 ### Registration
@@ -74,7 +101,7 @@ classes = (
 )
 
 def register():
-    for cl in classes:  
+    for cl in classes:
         bpy.utils.register_class(cl)
 
     bpy.types.Scene.variants = CollectionProperty(type=VariantItem)
